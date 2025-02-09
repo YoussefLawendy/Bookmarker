f9d057d5c2526f99aedb601dd464d6e6cafe8577
YoussefLawendy <youssefhanyelshahat@gmail.com>
2025-02-09 21:48:59 +0200

    Update Validation and Design

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,15 +1,28 @@
-const siteNameInput = document.getElementById("siteName");
-const siteURLInput = document.getElementById("siteURL");
-const siteContentInput = document.getElementById("siteContentInput");
+let siteNameInput = document.getElementById("siteName");
+let siteURLInput = document.getElementById("siteURL");
+let siteContentInput = document.getElementById("siteContentInput");
+let urlValidation = document.getElementById("URLValidation");
+let NameValidation = document.getElementById("nameValidation");
+const SubmitBtn = document.getElementById("submitBtn");
+const urlPattern =
+    /^(https?:\/\/)?(www\.)+[\da-z\.-]+\.[a-z]{2,6}([\/\w \.-]*)*\/?$/;
+
+let unfocusName = false;
+let unfocusURL = false;
 
 let allSites = JSON.parse(localStorage.getItem("allSites")) || [];
-
+SubmitBtn.disabled = true;
 function submitBtn() {
     const siteName = siteNameInput.value.trim();
     const siteURL = siteURLInput.value.trim();
 
     if (!siteName || !siteURL) {
-        alert("Please fill in both fields.");
+        Swal.fire({
+            icon: "error",
+            title: "invalid data",
+            text: "Please fill in both fields.",
+            confirmButtonColor: "#198754",
+        });
         return;
     }
 
@@ -21,7 +34,24 @@ function submitBtn() {
     if (
         allSites.some((s) => s.nameSite === siteName || s.nameURL === siteURL)
     ) {
-        alert("This site is already bookmarked.");
+        Swal.fire({
+            title: "This site is already bookmarked.",
+            icon: "warning",
+            showClass: {
+                popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+            `,
+            },
+            hideClass: {
+                popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+            `,
+            },
+        });
         return;
     }
 
@@ -33,7 +63,13 @@ function submitBtn() {
     localStorage.setItem("allSites", JSON.stringify(allSites));
     displaySites();
     clearForm();
-    alert("Bookmark added successfully!");
+
+    Swal.fire({
+        title: "Good job!",
+        text: "Bookmark added successfully!",
+        icon: "success",
+        confirmButtonColor: "#198754",
+    });
 }
 
 function displaySites() {
@@ -58,14 +94,34 @@ function displaySites() {
 
     siteContentInput.innerHTML = cartoona;
 }
-
 function deleteSite(index) {
-    if (confirm("Are you sure you want to delete this bookmark?")) {
-        allSites.splice(index, 1);
-        localStorage.setItem("allSites", JSON.stringify(allSites));
-        displaySites();
-        alert("Bookmark deleted successfully!");
-    }
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        iconHtml:
+            "<i class='fa-solid fa-trash' style='color: red; font-size: 30px;'></i>",
+        iconColor: "transparent",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#198754",
+        confirmButtonText: "Yes, delete it!",
+        customClass: {
+            icon: "swal2-icon swal2-icon-custom",
+        },
+    }).then((result) => {
+        if (result.isConfirmed) {
+            allSites.splice(index, 1);
+            localStorage.setItem("allSites", JSON.stringify(allSites));
+            displaySites();
+
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your bookmark has been deleted.",
+                icon: "success",
+                confirmButtonColor: "#198754",
+            });
+        }
+    });
 }
 
 function visitSite(index) {
@@ -81,9 +137,33 @@ function clearForm() {
     siteURLInput.value = "";
 }
 
+function URLValidation() {
+    if (unfocusURL) {
+        if (!urlPattern.test(siteURLInput.value)) {
+            urlValidation.innerText = "Invalid URL";
+            SubmitBtn.disabled = true;
+        } else {
+            urlValidation.innerText = "";
+            SubmitBtn.disabled = false;
+        }
+    }
+}
+function nameValidation() {
+    let siteNameValue = siteNameInput.value.trim();
+
+    if (unfocusName) {
+        if (siteNameValue.length < 3) {
+            NameValidation.innerText =
+                "Invalid Name: Minimum 3 characters required";
+            SubmitBtn.disabled = true;
+        } else {
+            NameValidation.innerText = "";
+            SubmitBtn.disabled = false;
+        }
+    }
+}
+
 function isValidURL(url) {
-    const urlPattern =
-        /^(https?:\/\/)?(www\.)?[\da-z\.-]+\.[a-z]{2,6}([\/\w \.-]*)*\/?$/;
     return urlPattern.test(url);
 }
 
